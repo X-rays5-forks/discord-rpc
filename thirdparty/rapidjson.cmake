0bdd2e96a509c3a046ccdbee7d6a7ae1a018e74f
X-ray <61073708+X-rays5@users.noreply.github.com>
2025-08-21 21:50:07 +0200

    Update rapidjson.cmake

--- thirdparty/rapidjson.cmake
+++ thirdparty/rapidjson.cmake
@@ -1,3 +1,4 @@
-cpmaddpackage("gh:Tencent/rapidjson#ab1842a2dae061284c0a62dca1cc6d5e7e37e346")
+cpmaddpackage("gh:Tencent/rapidjson#24b5e7a8b27f42fa16b96fc70aade9106cf7102f")
+
 
 target_include_directories(discord-rpc PRIVATE ${rapidjson_SOURCE_DIR}/include)
